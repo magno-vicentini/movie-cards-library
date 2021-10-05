c91281ef507e2af2d35e57cb71540b51ac86da85
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-10-05 13:18:14 -0300

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here
+import React,{ Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <header>
+      <h1>Movie Cards</h1>
+    </header>
+  }
+}
+
+export default Header
